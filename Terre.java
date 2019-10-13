33b8c4bcba62c06a70e533acad668b9d7f370fc0
thorkel77 <46449323+thorkel77@users.noreply.github.com>
2019-10-14 01:26:44 +0200

    Update Terre.java

--- Terre.java
+++ Terre.java
@@ -3,6 +3,18 @@ public class Terre {
 
 	public static void main(String[] args) {
 		
+	System.out.println("Voici notre jardin:");
+	System.out.println("ooooo");
+	System.out.println("ooooo");
+	System.out.println("ooooo");
+	System.out.println("Et notre panier contient:");
+	
+	Jardin j = new Jardin(10,5);
+	j.ajouterPanier();
+	
+	System.out.println(j);
+	
+	
 
 	}
 
